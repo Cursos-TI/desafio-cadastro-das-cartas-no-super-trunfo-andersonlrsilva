415ffb82c89fb4e75e9c4de6f4befe4d9fc31480
andersonlrsilva <andersonlrsilva23@gmail.com>
2025-03-27 23:37:50 -0300

    Final version

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,7 +17,10 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-//coletando informações
+
+
+
+    //coletando informações
 
 char estado[50], nomecidade[50], codigocarta[50];
 int populacao, pontotur;
