a98dde6b3a3f6b6573028f805c66015d6b24a39f
andersonlrsilva <andersonlrsilva23@gmail.com>
2025-04-02 12:54:54 -0300

    comp

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -19,19 +19,20 @@ int main() {
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
 
-    //coletando informações
+    //Variaveis
 
-char estado[50], nomecidade[50], codigocarta[50];
-int populacao, pontotur;
-float area, pib, pibpercapt, densidade;
+char estado[50], nomecidade[50], codigocarta[50], estado2[50], nomecidade2[50],
+codigocarta2[50];
+int populacao, pontotur, populacao2, pontotur2;
+float area, pib, pibpercapt, densidade, area2, pib2, pibpercapt2, densidade2;
 
 
-
+//Obtendo dados carta 1
 
 
 printf(" *** CARTA 1 *** \n");
 printf("\n");
-
+//Entrada de Dados Carta 1
 // Nome do estado
 printf("insira o estado: \n");
 scanf("%s", &estado);
@@ -55,37 +56,99 @@ printf("Insira o numero de pontos turisticos: \n");
 scanf("%d", &pontotur);
 
 
+//Obtendo dados carta 2
+
+printf(" *** CARTA 2 *** \n");
+printf("\n");
+//Entrada de Dados Carta 1
+// Nome do estado
+printf("insira o estado: \n");
+scanf("%s", &estado2);
+// Codigo da carta
+printf("Insira o codigo da carta: \n");
+scanf("%s", &codigocarta2);
+// Nome da Cidade
+printf("Insira o nome da cidade: \n");
+scanf("%s", &nomecidade2);
+// Numero de habitantes
+printf("Insira o numero de habitantes: \n");
+scanf("%d", &populacao2);
+// Area total do estado
+printf("Insira a área do estado (em Km2): \n");
+scanf("%f", &area2);
+// PIB do estado
+printf("insira o PIB do estado: \n");
+scanf("%f", &pib2);
+// Numero de pontos turisticos
+printf("Insira o numero de pontos turisticos: \n");
+scanf("%d", &pontotur2);
+
+
 
 // Nível Aventureiro
-//Cálculos
+//Cálculos carta 1
 
 densidade = (float) populacao / area;
 pibpercapt =  pib / (float) populacao;
+float super = (float) populacao + area + pib + (float)pontotur + 
+densidade + pibpercapt;
+
+//Cálculos carta 2
+
+densidade2 = (float) populacao2 / area2;
+pibpercapt2 =  pib2 / (float) populacao2;
+float super2 = (float) populacao2 + area2 + pib2 + (float)pontotur2 + 
+densidade2 + pibpercapt2;
 
 
 
 
+
+// Imprimir informações da carta 1 na na tela
     printf("\n");
     printf(" *** CARTA 1 *** \n");
     printf("\n");
-// Imprimir informações da carta 1 na na tela
     printf("Estado: %s\n"
-        "Código: %s\n"
+        "Codigo: %s\n"
         "Nome da cidade: %s\n"
         "População: %d\n"
-        "Área: %.2f\n"
-        "PIB %.2f\n"
-        "Pontos Turitiscos %d\n"
-        "Densidade populacional %.2f\n"
-        "Pib Per Capita %.2f\n"
+        "Area: %.2f\n"
+        "PIB: %.2f\n"
+        "Pontos Turitiscos: %d\n"
+        "Densidade populacional: %.2f\n"
+        "Pib Per Capita: %.2f\n"
+        "Super Poder: %.2f\n"
         ,estado, codigocarta, 
-        nomecidade, populacao, area, pib, pontotur, densidade, pibpercapt );
+        nomecidade, populacao, area, pib, pontotur, densidade, 
+        pibpercapt, super );
 
 
 
 
 
-    
+    printf("\n");
+    printf(" *** CARTA 2 *** \n");
+    printf("\n");
+// Imprimir informações da carta 1 na na tela
+    printf("Estado: %s\n"
+            "Codigo: %s\n"
+            "Nome da cidade: %s\n"
+            "População: %d\n"
+            "Area: %.2f\n"
+            "PIB: %.2f\n"
+            "Pontos Turitiscos: %d\n"
+            "Densidade populacional: %.2f\n"
+            "Pib Per Capita: %.2f\n"
+            "Super Poder: %.2f\n"
+            ,estado2, codigocarta2, 
+            nomecidade2, populacao2, area2, pib2, pontotur2, densidade2, 
+            pibpercapt2, super2);
+
+
+
+
+
+    //comparação das cartas
     
 
 
